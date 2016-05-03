1c3cef7111fa67006978c4eab73e75e84e64ec80
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-05-03 15:55:09 -0300

    Added: [apImageRegister] Load, histogram and joint histogram methods.
    I made methods for histograms calculation and joint histogram calculation apply to fixed and moving images. apImageRegister use OpenCV functions.

--- apImageRegister.cpp
+++ apImageRegister.cpp
@@ -2,17 +2,112 @@
 
 using namespace ap;
 
-ImageRegister::ImageRegister()
+ImageRegister::ImageRegister(string fixed_path, string moving_path)
+  :histSize(255)
 {
-      
-}
-
-void ImageRegister::output()
-{
-    cout<<"Hello World!"<<endl;
+  getImages(fixed_path,moving_path);
 }
 
 ImageRegister::~ImageRegister()
 {
   
 }
+
+int ImageRegister::getImages(string fixed_path, string moving_path)
+{
+  
+  fixed=imread(fixed_path);
+  moving=imread(moving_path);  
+  
+  if (!fixed.data || !moving.data)
+  {
+    cerr<<"No image data available!"<<endl;
+    return FAILURE;
+  }
+  
+  Size size_fixed=fixed.size();
+  Size size_moving=moving.size();  
+  
+  if ((size_fixed.height*size_fixed.width)>(size_moving.height*size_moving.width)) 
+    resize(moving, moving, fixed.size());
+  else 
+    resize(fixed, fixed, moving.size());   
+  
+  return OK;
+}
+
+Mat ImageRegister::calHistogram(Mat image)
+{
+  Mat hist=Mat::zeros(1,histSize,CV_32FC1);
+  
+  if (image.channels()==3) cvtColor(image, image, CV_RGB2GRAY);
+  
+  float range[] = { 0, 256 };
+  const float* hist_range = range; 
+  
+  calcHist( &image, 1, 0, Mat(), hist, 1, &histSize, &hist_range );
+    
+  return hist;
+}
+
+Mat ImageRegister::calJointHistogram(Mat image_1, Mat image_2)
+{
+  /* TODO
+   * > Verify this calculation
+   * > Search a better method to do this.
+   */
+  Mat jpdf;
+  Size image_size=image_1.size();  
+  jpdf= Mat::zeros(histSize,histSize,CV_32FC1);
+
+  for (int i=0;i<image_size.height; i++)
+  {
+    for (int j=0;j<image_size.width; j++)
+    {
+      jpdf.at<float>(image_1.at<uchar>(i,j),image_2.at<uchar>(i,j))=(float)(jpdf.at<float>(image_1.at<uchar>(i,j),image_2.at<uchar>(i,j))+1);     
+    }    
+  }
+  
+  return jpdf;  
+}
+
+Mat ImageRegister::calEntropy(Mat image)
+{
+  /* TODO
+   * > Implement this method
+   */
+  Mat entropy; 
+  
+  return entropy;
+}
+
+Mat ImageRegister::calJointEntropy(Mat image_1, Mat image_2)
+{
+  /* TODO
+   * > Implement this method
+   */
+  Mat je; 
+  
+  return je;
+}
+
+Mat ImageRegister::calMutualInformation(Mat image_1, Mat image_2)
+{
+  /* TODO
+   * > Implement this method
+   */
+  Mat mi; 
+  
+  return mi;
+}
+
+double ImageRegister::calMaxMutualInformationValue(Mat image_1, Mat image_2)
+{
+  /* TODO
+   * > Implement this method
+   */
+  double max_mi; 
+  
+  return max_mi;
+}
+
--- apImageRegister.hpp
+++ apImageRegister.hpp
@@ -2,22 +2,45 @@
 #define apImageRegister_H
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 namespace ap
 {
   class ImageRegister {
+    
+    /* TODO
+     * > Verify what methods are public.
+     */
       
-    public:
-      ImageRegister();
-      ~ImageRegister();
-      void output();
+  public:
+    ImageRegister(string fixed_path,string moving_path);
+    ~ImageRegister();
+    int getImages(string fixed_path, string moving_path);
+    Mat calHistogram(Mat image);
+    Mat calJointHistogram(Mat image_1, Mat image_2);
+    Mat calEntropy(Mat image);
+    Mat calJointEntropy(Mat image_1, Mat image_2);
+    Mat calMutualInformation(Mat image_1, Mat image_2);
+    double calMaxMutualInformationValue(Mat image_1, Mat image_2);
       
-    public:
-      static const int OK=0;
-      static const int FAILURE=-1;
+    /* Testing*/    
+            
+  public:
+    static const int OK=0;
+    static const int FAILURE=-1;
+    
+  private:
+    Mat fixed, moving;
+    int histSize;
+    
+  public:
+    int getHistSize() const { return histSize; }
+    Mat getFixedImage() const { return fixed; }
+    Mat getMovingImage() const { return moving; }
 
-    private: 
+    /* Testing*/
       
   };
 }
